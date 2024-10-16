1725ca2560ac04401da54d77fecd4adbeaf9990c
Antelmo Verdugo <antelmogg@outlook.com>
2024-10-16 16:21:42 -0700

    Primeros cambios

---
+++ src/pages/login_page.jsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Form } from "@/components/ui/form"
+
+const formSchema = z.object({
+    username: z.string().min(2, {
+        message: "Username must be at least 2 characters.",
+    }),
+})
+
+export function ProfileForm() {
+    // 1. Define your form.
+    const form = useForm ({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
+    return (
+        <Form />
+    );
+}
