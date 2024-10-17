cf078e153c8c9e1176230b0d38c8beb67aa1573d
Antelmo Verdugo <antelmogg@outlook.com>
2024-10-16 17:16:41 -0700

    Formulario

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { ProfileForm } from "./pages/login_page"
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full h-screen flex flex-col justify-center items-center">
       <ProfileForm />
     </div>
   )
--- src/pages/login_page.jsx
+++ src/pages/login_page.jsx
@@ -12,10 +12,11 @@ import {
     FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 const formSchema = z.object({
-    name: z.string().min(8, {
-        message: "Nombre debe tener minimo 8 caracteres",
+    password: z.string().min(8, {
+        message: "La contraseña debe tener minimo 8 caracteres",
     }),
 })
 
@@ -24,7 +25,7 @@ export function ProfileForm() {
     const form = useForm  ({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            name: "",
+            email: "",
         },
     })
 
@@ -36,24 +37,117 @@ export function ProfileForm() {
     }
 
     return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Nombre</FormLabel>
-                            <FormControl>
-                                <Input placeholder="shadcn" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit">Submit</Button>
-            </form>
-        </Form>
+        <Tabs defaultValue="login" className="w-11/12 sm:w-8/12 xl:w-6/12">
+            <TabsList className="w-full">
+                <TabsTrigger className="w-full" value="login">Inicio de Sesión</TabsTrigger>
+                <TabsTrigger className="w-full" value="register">Registro</TabsTrigger>
+            </TabsList>
+            <TabsContent value="login">
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Correo</FormLabel>
+                                    <FormControl>
+                                        <Input type="email" placeholder="correo@test.com" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Contraseña</FormLabel>
+                                    <FormControl>
+                                        <Input type="password" placeholder="" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit">Iniciar Sesión</Button>
+                    </form>
+                </Form>
+            </TabsContent>
+            <TabsContent value="register">
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Nombre</FormLabel>
+                                    <FormControl>
+                                        <Input type="text" placeholder="Pedro" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="last_name_1"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Primer apellido</FormLabel>
+                                    <FormControl>
+                                        <Input type="text" placeholder="Perez" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="last_name_2"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Segundo apellido</FormLabel>
+                                    <FormControl>
+                                        <Input type="text" placeholder="Perez" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Correo</FormLabel>
+                                    <FormControl>
+                                        <Input type="email" placeholder="correo@test.com" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Contraseña</FormLabel>
+                                    <FormControl>
+                                        <Input type="password" placeholder="" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit">Registrarme</Button>
+                    </form>
+                </Form>
+            </TabsContent>
+        </Tabs>
     )
 }
 
